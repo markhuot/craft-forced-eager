893924ed827a9efb2b168b27fc13e13a789cef5e
markhuot <mark@markhuot.com>
2023-02-10 17:10:50 -0500

    support for super table and avoiding CP requests

--- src/Eager.php
+++ src/Eager.php
@@ -9,6 +9,8 @@ use craft\fields\BaseRelationField;
 use craft\fields\Matrix;
 use markhuot\forcedeager\base\ErrorProxy;
 use markhuot\forcedeager\behaviors\AllowLazyRelationsBehavior;
+use verbb\supertable\elements\db\SuperTableBlockQuery;
+use verbb\supertable\fields\SuperTableField;
 use yii\base\BootstrapInterface;
 use yii\base\Event;
 
@@ -17,19 +19,26 @@ class Eager implements BootstrapInterface
     function bootstrap($app)
     {
         Event::on(ElementQuery::class, ElementQuery::EVENT_DEFINE_BEHAVIORS, function (DefineBehaviorsEvent $event) {
-            $event->behaviors[] = AllowLazyRelationsBehavior::class;
+            $event->behaviors['AllowLazyRelationsBehavior'] = AllowLazyRelationsBehavior::class;
         });
 
         Event::on(ElementQuery::class, ElementQuery::EVENT_AFTER_POPULATE_ELEMENT, function (PopulateElementEvent $event) {
-            /** @var AllowLazyRelationsBehavior $query */
-            $query = $event->sender;
-            if ($query->lazyRelationsAreAllowed()) {
+            if (\Craft::$app->request->isConsoleRequest) {
                 return;
             }
 
+            if (!\Craft::$app->request->isSiteRequest) {
+                return;
+            }
+
+            /** @var AllowLazyRelationsBehavior $query */
+            $query = $event->sender;
+            if (!is_a($query, SuperTableBlockQuery::class) && $query->lazyRelationsAreAllowed()) {
+                return;
+            }
 
             foreach ($event->element->getFieldLayout()->getCustomFields() as $field) {
-                if (is_subclass_of($field, BaseRelationField::class) || is_a($field, Matrix::class)) {
+                if (is_subclass_of($field, BaseRelationField::class) || is_a($field, Matrix::class) || is_a($field, SuperTableField::class)) {
                     $event->element->setEagerLoadedElements($field->handle, [new ErrorProxy($event->sender, $event->element, $field)]);
                 }
             }
